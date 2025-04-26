32955584aa39919462110db7f150b072e3ea44d8
dixxanta08 <dixantashrestha08@gmail.com>
2025-04-26 09:25:41 +0545

    Add Hello.js NPM INIT TEST.JS AND WORKFLOW

--- hello.js
+++ hello.js
@@ -1,13 +1,5 @@
-// a simple js for git practing
-function hello() {
-    console.log('hello world');
+function add(a, b) {
+    return a + b;
 }
 
-console.log("hello world");
-
-// urgent bug fix
-// I am in the process of implementing a simple function that returns "hello world"
-// code
-// code
-// code
-
+module.exports = { add };
